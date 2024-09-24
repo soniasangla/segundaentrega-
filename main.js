611ca70a40e6a73b7c5a3c8bf6b6ea45dd6b433b
soniasangla <soniasangla@gmail.com>
2024-09-23 22:50:33 -0300

    js

---
+++ main.js
@@ -0,0 +1,39 @@
+
+HOLA Pr ofe TUVE UN PROBLEMA Y NO PUDE TERMINAR LA ENTREGA. MAÑANA HARE UN PUSH
+
+
+/*const botones = document.querySelectorAll('.masinfo');
+botones.forEach((boton, index) => {
+    boton.addEventListener('click', function() {
+        let infoProducto;
+        switch(index) {
+            case 0:
+                infoProducto = 'Información: Pedigree para Perro Adulto.';
+                break;
+            case 1:
+                infoProducto = 'Información: Dogchow para Perro Joven.';
+                break;
+            case 2:
+                infoProducto = 'Información: Bocadito filetes de Pollo.';
+                break;
+            case 3:
+                infoProducto = 'Información: Whiskas para Gato Adulto.';
+                break;
+            case 4:
+                infoProducto = 'Información: Bocadito para Gato.';
+                break;
+            case 5:
+                infoProducto = 'Información: Proplan para Perro.';
+                break;
+            default:
+                infoProducto = 'Información no disponible.';
+        }
+        alert(infoProducto);
+    });
+});
+
+
+
+
+
+
